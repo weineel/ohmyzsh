a21896c85553329cc894299f17d6c7f67346ba74
weineel <LiJF_wn@163.com>
2020-10-24 11:15:18 +0800

    feat(weineel): 添加 copydir、copyfile 命令

--- custom/plugins/weineel/weineel.plugin.zsh
+++ custom/plugins/weineel/weineel.plugin.zsh
@@ -1,4 +1,17 @@
-alias ppc='pwd | pbcopy'
 alias gbdm='git branch -d $(git branch --merged | grep -v -E "develop|master") '
 alias gcza='git add . && git cz'
+
+# Copies the pathname of the current directory to the system or X Windows clipboard
+function copydir {
+  emulate -L zsh
+  print -n $PWD | clipcopy
+}
+
+# Copies the contents of a given file to the system or X Windows clipboard
+#
+# copyfile <file>
+function copyfile {
+  emulate -L zsh
+  clipcopy $1
+}
 
